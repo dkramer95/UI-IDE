a0d62d23929fa0563804b0ad36e4300506ec7357
alphaponchitovtrix <ponchitovazquez@gmail.com>
2017-04-17 11:17:41 -0600

    // pushing the start of HTMLElement.java

---
+++ src/client/HTMLElement.java
@@ -0,0 +1,21 @@
+package client;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+
+import lib.UIElement;
+
+public class HTMLElement extends UIElement {
+
+	@Override
+	public void draw(Graphics2D g) {
+		
+	}
+
+	@Override
+	public String generateSourceCode() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
