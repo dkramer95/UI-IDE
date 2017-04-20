d8dd7c239e8b9e82e023372186ae7ef9a23fdeb8
alphaponchitovtrix <ponchitovazquez@gmail.com>
2017-04-20 10:02:20 -0600

    //commit added temp csc style lines added class absolute

--- src/lib/HTMLWriter.java
+++ src/lib/HTMLWriter.java
@@ -9,9 +9,21 @@ public class HTMLWriter extends ElementWriter{
 	private void Init() {
 		text.add("<!DOCTYPE html>");
 		text.add("<html>");
-		text.add("<head></head>");
-		text.add("<head></head>");
-		text.add("<body>");
+		text.add("<head>");
+		text.add("<style>");
+		text.add("body.relative {");
+		text.add("position: relative;");
+		text.add("width: 800px;");
+		text.add("height: 800px;");
+		text.add("border: 3px solid #73AD21;");
+		text.add("}");
+		text.add("</style>");
+		text.add("</head>");
+		text.add("<body class=\"relative\">");
+	}
+	
+	public void addStyle(String line){
+		
 	}
 	
 	@Override
